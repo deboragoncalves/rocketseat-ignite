f0ccb522d12d7d8aa3ea76d569a07c4b3b9ccb3e
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-09-27 19:16:58 -0300

    Ignite Feed: Estilizar componente Post

--- classes/ignite-feed/src/components/Post.jsx
+++ classes/ignite-feed/src/components/Post.jsx
@@ -5,6 +5,7 @@ export function Post() {
         - No HTML, atributo com nome composto é separado por hifen, já no React o padrão é camelCase
         - Tag tag possui o atributo dateTime, recebendo uma string no padrão YYYY-MM-DD HH:mm:ss
         - Tag time também pode ter o atributo title (similar ao alt no img), exibindo um texto auxiliar ao passar o mouse pelo conteúdo
+        - Espaçamento entre elementos HTML dentro do JSX: pode ser feito com CSS ou adicionando um espaço entre chaves {'  '}
     */
     return (
         <article className={styles.post}>
@@ -19,7 +20,7 @@ export function Post() {
                 <time title="27 de setembro às 17:43" dateTime="2022-09-27 16:40:00">Publicado há 1h</time>
             </header>
             <div className={styles.content}>
-                <p>Conteúdo Post</p>
+                <p>Text post</p>
             </div>
         </article>
     )
--- classes/ignite-feed/src/components/Sidebar.jsx
+++ classes/ignite-feed/src/components/Sidebar.jsx
@@ -1,7 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
 
-
 export function Sidebar() {
     return (
         <>
